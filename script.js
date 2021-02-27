0cf75294ff05148b4ebce0e95c1cd46d054d2585
Artem Syropiatov <syropyatov.0000@gmail.com>
2021-02-27 15:09:34 +0200

    repared index.html and script.js with done 15 vid

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-"use strict";
+/* Задание на урок:
 
-// if (4 == 4) {
-//     console.log('True');
-// } else {
-//     console.log('False');
-// }
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-const num = 52;
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-// if (num < 49) {
-//     console.log('Err');
-// } else if (num > 100) {
-//     console.log('Too much');
-// } else {
-//     console.log('Ok!');
-// }
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
 
 
-// (num == 50) ? console.log('True'): console.log('False');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
-switch (num) {
-    case 50:
-        console.log('True');
-        break;
-
-    case 49:
-        console.log('False');
-        break;
-
-    default:
-        console.log('Not today...');
-        console.log('But you can try nex time!');
-        break;
+for (let i = 0; i < numberOfFilms; i++) {
+    let movieName = prompt('Один из последних просмотренных фильмов?', '');
+    let movieRating = prompt('На сколько оцените его?', '');
+    if (movieName != null && movieName != null && movieName.length < 50 && movieName.length < 50 && movieName != '' && movieName != '') {
+        personalMovieDB.movies[movieName] = movieRating;
+    } else {
+        i--;
+        continue;
+    }
 }
+
+console.log(personalMovieDB);
