3c53d5011f04ccd27a144eddbaa2b30adaa85711
Artem Syropiatov <syropyatov.0000@gmail.com>
2021-03-07 22:00:21 +0200

    script with OOP basics

--- script.js
+++ script.js
@@ -1,102 +1,31 @@
 "use strict";
 
-let a = 5;
-let b = a;
+let str = "some";
+let strObj = new String(str);
 
-b = b + 5;
+console.log(typeof (str));
+console.log(typeof (strObj));
 
-console.log(b);
-console.log(a);
 
-const obj = {
-    a: 5,
-    b: 1
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armour: 100
 };
 
-const copy = obj; // в   *copy*   передаётся ссылка на   *obj*
+const john = Object.create(soldier);
 
-copy.a = 10;
+// const john = {
+//     health: 100
+// };
 
-console.log(copy);
-console.log(obj);
+// john.__proto__ = soldier;   // Устаревший синтаксис
 
-function copyObj(mainObj) {
-    let objCopy = {};
+// Object.setPrototypeOf(john, soldier);
 
-    for (let key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-};
-
-const newNumbers = copyObj(numbers);
-
-console.log(newNumbers);
-
-const add = {
-    d: 17,
-    i: 20
-};
-
-console.log(Object.assign(numbers, add));
-
-const clone = Object.assign({}, numbers);
-
-clone.c.x = 128;
-clone.d = 128;
-
-console.log(clone);
-console.log(numbers);
+// console.log(john.armour);
 
 
-console.log("----------------------------------------------");
-
-const oldArr = ['a', 'b', 'c', ['d', 'e']];
-const newArr = oldArr.slice();
-
-newArr[1] = 'asdasd';
-newArr[3][1] = 'ccccc';
-console.log(newArr);
-console.log(oldArr);
-
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    intenet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log("-----------------------------------------");
-console.log(intenet);
-
-function logger(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 25, 5, 7, 13];
-
-logger(...num);
-
-const array = ["a", "b"];
-const newArray = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {
-    ...q
-};
-
-console.log("-----------------------------------");
-console.log(newObj);
+console.log(john);
+console.log(john.armour);
